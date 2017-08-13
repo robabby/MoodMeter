3424d2a78db4b76ea84510bbbe23bab0703fcc63
robabby <robabby23@gmail.com>
2017-08-12 19:35:08 -0500

    Completed Welcome Flow

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import ReviewScreen from  './screens/ReviewScreen';
+
+export default class App extends React.Component {
+  render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        })
+      }
+    });
+
+    return (
+      <MainNavigator />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+  { text: 'Welcome to Job App', color: '#03a9f4' },
+  { text: 'Use this to get a job', color: '#009688' },
+  { text: 'Set your location, then swipe away', color: '#03a9f4' }
+];
+
+class WelcomeScreen extends Component {
+  onSlidesComplete = () => {
+    this.props.navigation.navigate('auth');
+  }
+
+  render() {
+    return (
+      <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete} />
+    );
+  }
+}
+
+export default WelcomeScreen;
