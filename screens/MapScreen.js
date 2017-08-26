e9b1077e7bf19f9d0c3f5cef739d98e8e7da7214
robabby <robabby23@gmail.com>
2017-08-26 13:10:50 -0500

    Implemented findJobs action

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
+import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
+
+import * as actions from '../actions';
 
 class MapScreen extends Component {
   state = {
+    mapLoaded: false,
     region: {
       longitude: -122,
       latitude: 37,
@@ -12,16 +17,59 @@ class MapScreen extends Component {
     }
   }
 
+  componentDidMount() {
+    this.setState({ mapLoaded: true });
+  }
+
+  onRegionChangeComplete = (region) => {
+    this.setState({ region });
+  }
+
+  onButtonPress = () => {
+    // You get access to action creators through the props of your component
+    this.props.fetchJobs(this.state.region);
+  }
+
   render() {
+    if (!this.state.mapLoaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
     return (
       <View style={{ flex: 1 }}>
         <MapView
           region={this.state.region}
           style={{ flex: 1 }}
+          onRegionChangeComplete={this.onRegionChangeComplete}
         />
+        <View style={styles.buttonContainer}>
+          <Button
+            large
+            title="Search This Area"
+            backgroundColor="#009688"
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     );
   }
 }
 
-export default MapScreen;
+styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left:0,
+    right: 0
+  }
+}
+
+// pulling in all /actions/ for ease of use
+// can also destructure /actions/ with { fetchJobs }
+// for access to a specific action as long as the import
+// statement is updated as well
+export default connect(null, actions)(MapScreen);
